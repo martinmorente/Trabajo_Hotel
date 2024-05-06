090f09d9a1d127bc14ee4811540a101c5ab562bf
zRaonn <148863644+zRaonn@users.noreply.github.com>
2024-05-06 12:29:19 +0200

    Update ServicioExtraRepository.java

--- ServicioExtraRepository.java
+++ ServicioExtraRepository.java
@@ -1,3 +1,9 @@
-public interface ServicioExtraRepository extends JpaRepository<ServicioExtra, Long> {
+package com.example.demo.repositories;
 
+import com.example.demo.models.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ServicioExtraRepository extends JpaRepository<ServicioExtra, Long> {
+        
 }    
