316b80e1b1042124fca03d39c10bd28335fb6282
martinmorente <148863624+martinmorente@users.noreply.github.com>
2024-05-06 17:37:34 +0200

    Delete Models directory

--- Models/personalRepository.java
+++
@@ -1,7 +0,0 @@
-
-package com.guarderiaJunior.Models;
-
-import com.guarderiaJunior.Clases.Personal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface personalRepository extends JpaRepository<Personal, Long> {}
