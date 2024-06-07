d4e8c9cb89b07fd3e5e08361072856ff589df242
martinmorente <martinmorente09@digitechfp.com>
2024-06-07 09:08:33 +0200

    Hotel(Actualizado)

--- Hotel/src/main/java/com/guarderiaJunior/Hotel/Models/reservaRepository.java
+++
@@ -1,7 +0,0 @@
-
-package com.guarderiaJunior.Hotel.Models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.guarderiaJunior.Hotel.Clases.Reservas;
-
-public interface reservaRepository extends JpaRepository<Reservas,Long> {}
