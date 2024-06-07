cdede7427f521ec6ed385c1bdbd094ab76a152b8
martinmorente <martinmorente09@digitechfp.com>
2024-06-07 09:06:14 +0200

    Hotel(Actualizado)

--- ServicioExtraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ServicioExtraRepository extends JpaRepository<ServicioExtra, Long> {
-        
-}    
